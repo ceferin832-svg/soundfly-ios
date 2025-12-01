2002bb0793a64b757f90f568f4b60a61deb1d725
CeferinSoftware <newshellweb@gmail.com>
2025-12-01 18:04:30 +0100

    Add Safari iOS User-Agent to bypass Cloudflare verification

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -44,6 +44,9 @@ class _HomeScreenState extends State<HomeScreen> {
             : JavaScriptMode.disabled,
       )
       ..setBackgroundColor(AppTheme.white)
+      ..setUserAgent(
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+      )
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageStarted: (String url) {
